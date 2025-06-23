30ad56a8dcba89c2abcead7f3d8fc6329c0542c4
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2025-06-23 10:17:02 +0530

    Update header-guest.php

--- includes/header-guest.php
+++ includes/header-guest.php
@@ -1,7 +1,16 @@
-<header class="absolute top-0 left-0 w-full z-10 px-6 py-4 flex justify-between items-center bg-white/10 backdrop-blur-md border-b border-white/10">
-  <h1 class="text-2xl font-bold text-white">📚 Digital Study Library</h1>
+<header class="absolute h-20 top-0 left-0 w-full z-10 px-6 py-4 flex justify-between items-center bg-white/80 p-4 shadow-lg z-20 backdrop-blur-md border-black-300 rounded">
+  <h1 class="text-2xl font-bold text-black flex items-center space-x-2">
+  <img src="assets\img\logo.png" alt="Logo" class="h-6 w-6">
+  <span>ReserveSpot</span>
+</h1>
+
   <nav class="space-x-4">
-    <a href="/digital-library-portal/login.php" class="bg-blue-600 px-4 py-2 rounded hover:bg-blue-700 text-white">Login</a>
-    <a href="/digital-library-portal/register.php" class="bg-green-500 px-4 py-2 rounded hover:bg-green-600 text-white">Register</a>
+<a href="/digital-library-portal/login.php"
+   class="bg-[#131e5a] hover:bg-[#048C9E] text-white font-semibold py-2 px-4 rounded shadow-[0_6px_6px_-1px_rgba(0,0,0,0.4)] transition duration-300">
+   Login
+</a>
+
+
+    <a href="/digital-library-portal/register.php" class="bg-green-500 hover:bg-[#048C9E] text-white font-semibold py-2 px-4 rounded shadow-[0_6px_6px_-1px_rgba(0,0,0,0.4)] transition duration-300">Sign Up</a>
   </nav>
 </header>
